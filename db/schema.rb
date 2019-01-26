7afd4d79739fc163f2b2273ae1315da803dd533c
alex <alexyndr@gmail/com>
2019-01-26 04:05:58 +0200

    worked version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_204814) do
+ActiveRecord::Schema.define(version: 2019_01_26_015932) do
 
   create_table "pictures", force: :cascade do |t|
     t.string "image"
